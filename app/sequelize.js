c6fd12cbc5642c45436010637d3fa94f3b3b2692
jasondeving <aznj50@hotmail.com>
2016-10-15 18:33:39 -0700

    modified

--- app/sequelize.js
+++ app/sequelize.js
@@ -1,4 +1,8 @@
 var Sequelize = require('sequelize'),
+	// sequelize = new Sequelize('database', 'user', 'password', {
+ //    	host: 'localhost',
+ //    	dialect: 'mysql'
+ //    })
     sequelize = new Sequelize('login', 'root', '', {
     	host: 'localhost',
     	dialect: 'mysql'
